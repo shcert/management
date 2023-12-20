21ab4d5f353c24a1032de25e1c94796a1d1c913d
shcert <dbstjdghks25@gmail.com>
2023-12-20 08:59:16 -0500

    complete error edit

--- start.sh
+++ start.sh
@@ -1,6 +1,13 @@
 mkdir conf
-mkdir db
+
 podman network create management-network
+podman volume create grafana-data
+podman volume create influxdb-data
+podman volume create organizr-data
+podman volume create registry-web-data
+podman volume create registry-srv-data
+podman volume create registry-srv-auth
+
 openssl req -new -newkey rsa:4096 -days 365 -subj "/CN=localhost" \
         -nodes -x509 -keyout conf/auth.key -out conf/auth.cert
 podman-compose up -d
