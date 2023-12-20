b3e32f437c190f7f480a46b1320b76a62b062583
shcert <dbstjdghks25@gmail.com>
2023-12-20 08:05:04 -0500

    cannot build error edit

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 mkdir conf
+mkdir db
+podman network create management-network
 openssl req -new -newkey rsa:4096 -days 365 -subj "/CN=localhost" \
         -nodes -x509 -keyout conf/auth.key -out conf/auth.cert
 podman-compose up -d
